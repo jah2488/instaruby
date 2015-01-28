a0862866a047e79f25b2fe8f79492e6e364884d7
Justin Herrick <justin@justinherrick.com>
2015-01-28 16:14:51 -0600

    We made instagram.

---
+++ app/assets/javascripts/posts.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+	$('span.glyphicon.like-icon').on('click', function (e) {
+	  var currentElement = this;
+	  $.ajax({
+	  	url: '/posts/' + $(currentElement).data('id') + '/like',
+	  	method: 'put'
+	  }).done(function (response) {
+	  	$(currentElement)
+	  	    .parent()
+	  	    .find('.like-count')
+		  	.html(response.likes)
+		  	.addClass('flash');
+
+		setTimeout(function () {
+			$(currentElement)
+				.parent()
+				.find('.like-count')
+				.removeClass('flash');
+		}, 1000);
+	  });
+	});	
+});
