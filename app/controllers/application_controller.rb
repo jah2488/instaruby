bdb9c9037d5fdf0fdb1b9bad692dcb584de8a38a
Justin Herrick <justin@justinherrick.com>
2015-01-29 13:56:09 -0600

    booooo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
   before_filter :cors_allow_all
-  after_filter :cors_allow_all
+  after_filter :cors_allow_all_after
 
   def cors_allow_all
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
     headers['Access-Control-Request-Method'] = '*'
-    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Token, X-Requested-With, X-Prototype-Version'
+    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Length, X-Requested-With, Content-Type, Accept, Authorization, Token, X-Requested-With, X-Prototype-Version'
     headers['Access-Control-Max-Age'] = '1728000'
+  end
+
+  def cors_allow_all_after
+    if request.method == :options
+      cors_allow_all
+      render :text => '', :content_type => 'text/plain'
+    end
   end
 end
