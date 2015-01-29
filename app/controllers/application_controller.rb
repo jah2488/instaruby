3856a68b0806a0654478a006d7e52a03ffdbb399
Justin Herrick <justin@justinherrick.com>
2015-01-29 14:08:30 -0600

    maybe?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,18 +7,4 @@ class ApplicationController < ActionController::Base
   before_filter :cors_allow_all
   after_filter :cors_allow_all_after
 
-  def cors_allow_all
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-    headers['Access-Control-Request-Method'] = '*'
-    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Length, X-Requested-With, Content-Type, Accept, Authorization, Token, X-Requested-With, X-Prototype-Version'
-    headers['Access-Control-Max-Age'] = '1728000'
-  end
-
-  def cors_allow_all_after
-    if request.method == :options
-      cors_allow_all
-      render :text => '', :content_type => 'text/plain'
-    end
-  end
 end
