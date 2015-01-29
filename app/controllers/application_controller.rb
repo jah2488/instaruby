2dd5a26418cb21c78d268cea0398f066f66086b9
Justin Herrick <justin@justinherrick.com>
2015-01-29 13:46:14 -0600

    Disabling outgoing cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :cors_allow_all
+  after_filter :cors_allow_all
 
   def cors_allow_all
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
     headers['Access-Control-Request-Method'] = '*'
-    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Token, X-Requested-With, X-Prototype-Version'
+    headers['Access-Control-Max-Age'] = '1728000'
   end
 end
